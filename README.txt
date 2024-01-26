158b28d2c290a153c193191ea698babccede6dde
Alex Clark <aclark@aclark.net>
2024-01-26 18:29:24 -0500

    Update newzope

---
+++ README.txt
@@ -0,0 +1,17 @@
+
+Newzope
+=======
+
+This is the newzope "instance manager" for Plone, aimed at providing 
+rapid prototyping ability for those who like to fiddle. 
+It assumes you have Zope and Plone installed already. 
+
+If you don't like to fiddle (i.e. you are "human") you should probably 
+be using buildout (http://plone.org/documentation/tutorial/buildout).
+
+See 'newzope -h' for more info.
+
+Build instructions
+==================
+
+See INSTALL.txt
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='newzope',
+      version=version,
+      description="Make it easier to create and run Plone buildouts.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Alex Clark',
+      author_email='aclark@aclark.net',
+      url='http://svn.plone.org/svn/collective/newzope/trunk/',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
